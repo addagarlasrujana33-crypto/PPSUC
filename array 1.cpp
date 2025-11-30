0f3d16dd7eefa1424e02392fd055b2b895f51171
addagarlasrujana33-crypto <addagarlasrujana33@gmail.com>
2025-11-30 17:40:24 +0530

    Add files via upload

---
+++ array 1.cpp
@@ -0,0 +1,15 @@
+//to write the c program of array 
+#include<stdio.h>
+int main()
+{
+	int i;
+	int marks[6]={12, 13, 14, 15, 18};
+	//printing elements of array
+	printf("%d\n", marks[0]);
+	printf("%d\n", marks[1]);
+	printf("%d\n", marks[2]);
+	printf("%d\n", marks[3]);
+	printf("%d\n", marks[4]);
+	printf("%d\n", marks[5]);
+	return 0;
+}
