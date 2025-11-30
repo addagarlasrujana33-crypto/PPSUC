b4146e854f1a97bde7dd8ba9b7f08e02678ccd42
addagarlasrujana33-crypto <addagarlasrujana33@gmail.com>
2025-11-30 17:37:19 +0530

    Add files via upload

---
+++ arithmetic.c
@@ -0,0 +1,12 @@
+//to prform arithmetic operator
+#include<stdio.h>
+int main()
+{
+	int a=10,b=8;
+	printf("a+b=%d\n",a+b);
+	printf("a-b=%d\n",a-b);
+	printf("a*b=%d\n",a*b);
+	printf("a/b=%d\n",a/b);
+	printf("a%b=%d\n",a%b);
+	return 0;
+}
