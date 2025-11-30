04b57d03cd38b133edefd2c16398c1ca42869020
addagarlasrujana33-crypto <addagarlasrujana33@gmail.com>
2025-11-30 20:21:52 +0530

    Add files via upload

---
+++ positive number in if statement.cpp
@@ -0,0 +1,14 @@
+//to check the given number is positve in if statement
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the value of n");
+	scanf("%d", &n);
+	if(n>=0)
+	{
+		printf("given number is positive");
+	}
+	return 0;
+}
+
