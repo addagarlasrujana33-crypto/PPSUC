063d9ec91018ac7cdf99a03ad9222cf16f49b873
addagarlasrujana33-crypto <addagarlasrujana33@gmail.com>
2025-11-30 17:38:48 +0530

    Add files via upload

---
+++ array of elements using for loop.cpp
@@ -0,0 +1,12 @@
+//to write c program of array using for loop
+#include<stdio.h>
+int main()
+{
+	int i;
+	int marks[6]={12, 13, 14, 15, 16, 19};
+	for(i=0;i<=5;i++)
+	{
+		printf("%d\n", marks[i]);
+	}//end of for loop
+	return 0;
+}
