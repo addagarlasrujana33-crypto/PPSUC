cb51411dfee65f7d7747c93b8a20f79eeacd6f64
addagarlasrujana33-crypto <addagarlasrujana33@gmail.com>
2025-11-30 19:17:27 +0530

    Add files via upload

---
+++ eligible to vote in if else statement.cpp
@@ -0,0 +1,15 @@
+//to check the eligibility of vote
+#include<stdio.h>
+int main()
+{
+	int x=18;
+	if(x>=18)
+	{
+		printf("eligible to vote");
+	}
+	else
+	{
+		printf("not eligible to vote");
+	}
+	return 0;
+}
