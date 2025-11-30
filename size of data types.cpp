05220e970eb0175cc5e8ca2aa084118b5aba5802
addagarlasrujana33-crypto <addagarlasrujana33@gmail.com>
2025-11-30 20:26:12 +0530

    Add files via upload

---
+++ size of data types.cpp
@@ -0,0 +1,17 @@
+//to perform size of data type
+#include<stdio.h> 
+int main()
+{
+//use size of() to konwn size
+//the data types
+int a;
+float b;
+printf("the size of int:%d\n",sizeof(int));
+printf("the size of char:%d\n",sizeof(char));
+printf("the size of float:%d\n",sizeof(float));
+printf("the size of double:%d",sizeof(double));
+printf("the size of variable a is:%d",sizeof(a));
+printf("the size of variable b is:%d",sizeof(b));
+return 0;
+}
+
