77373335bb2dc90bbba5b39f879850dc0f8d667b
addagarlasrujana33-crypto <addagarlasrujana33@gmail.com>
2025-11-30 19:19:14 +0530

    Add files via upload

---
+++ explict conversion.cpp
@@ -0,0 +1,10 @@
+//to perform explicit conversion
+#include<stdio.h>
+int main()
+{
+	double x=1.2;
+	//explicit conversion from double to int
+	int sum=(int)x+1;
+	printf("sum=%d",sum);
+	return 0;
+}
