272efb608aa4096f0fd5703966ab605c07b7c363
addagarlasrujana33-crypto <addagarlasrujana33@gmail.com>
2025-11-30 17:41:03 +0530

    Add files via upload

---
+++ assignment operator.cpp
@@ -0,0 +1,19 @@
+//to perform assignment operator
+#include<stdio.h>
+int main()
+{
+	int a=10,b;
+	b=a;
+	printf("%d\n",b);
+	b+=a;
+	printf("%d\n",b);
+	b-=a;
+	printf("%d\n",b);
+	b*=a;
+	printf("%d\n",b);
+	b/=a;
+	printf("%d\n",b);
+	b%=a;
+	printf("%d\n",b);
+	return 0;
+}
