84e01f2d880cfe2c9d98c467597fa69d517b59e0
addagarlasrujana33-crypto <addagarlasrujana33@gmail.com>
2025-11-30 20:22:17 +0530

    Add files via upload

---
+++ positive number in if else statement.cpp
@@ -0,0 +1,17 @@
+//to check whether the given number is positive or negative in if else statement
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the n value");
+	scanf("%d", &n);
+	if(n>=0)
+	{
+		printf("the n value is positive");
+	}
+	else
+	{
+		printf("the n value is negative");
+	}
+	return 0;
+}
