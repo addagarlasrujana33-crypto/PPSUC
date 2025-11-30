fb8aa65cdc9b0237280c24bea67e09628558824c
addagarlasrujana33-crypto <addagarlasrujana33@gmail.com>
2025-11-30 17:44:44 +0530

    Add files via upload

---
+++ circumference of circle.cpp
@@ -0,0 +1,11 @@
+//to perform circumference of circle
+#include<stdio.h>
+main()
+{
+	int r=45;
+	float pi=3.14;
+	float circumference;
+	circumference=2*pi*r;
+	printf("circumference of circle:%f",circumference);
+	return 0;
+}
