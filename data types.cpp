ee89442f94a3699ef63161c243b8af884ec2fcb2
addagarlasrujana33-crypto <addagarlasrujana33@gmail.com>
2025-11-30 19:14:02 +0530

    Add files via upload

---
+++ data types.cpp
@@ -0,0 +1,23 @@
+//to perform data types
+#include<stdio.h>
+int main()
+{
+	int a;
+	float b;
+	char c;
+	double z;
+	printf("enter a value");
+	scanf("%d",&a);
+	printf("integer value is:%d",a);
+	printf("enter b value");
+	scanf("%f",&b);
+	printf("float value is:%f",b);
+	printf("enter c value");
+	scanf("%c",&c);
+	printf("char value is:%c",c);
+	printf("enter z value");
+	scanf("%lf",&z);
+	printf("double value is:%lf",z);
+	return 0;
+}
+
