86d238e15dbfa5a21048693e210270af4942383e
addagarlasrujana33-crypto <addagarlasrujana33@gmail.com>
2025-11-30 20:24:53 +0530

    Add files via upload

---
+++ primitive data types.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a=10;
+	char b='s';
+	float c=2.8;
+	double d=28.88;
+	printf("integer data type:%d", a);
+	printf("character data type:%c", b);
+	printf("float data type:%f", c);
+	printf("double data type:%lf", d);
+	return 0;
+}
