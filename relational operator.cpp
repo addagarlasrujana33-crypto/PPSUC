3212594a99f03e1b3e41afd7c060b260eb91eadf
addagarlasrujana33-crypto <addagarlasrujana33@gmail.com>
2025-11-30 20:25:42 +0530

    Add files via upload

---
+++ relational operator.cpp
@@ -0,0 +1,19 @@
+//to perform relational operators
+#include<stdio.h>
+int main()
+{
+	int a=5,b=4,c=10;
+	printf("%d\n",a==b);
+	printf("%d\n",a==c);
+	printf("%d\n",a>b);
+	printf("%d\n",a>c);
+	printf("%d\n",a<b);
+	printf("%d\n",a<c);
+	printf("%d\n",a!=b);
+	printf("%d\n",a!=c),
+	printf("%d\n",a>=b);
+	printf("%d\n",a>=c);
+	printf("%d\n",a<=b);
+	printf("%d\n",a<=c);
+	return 0;
+}
