c67b96aff2aa291a01d798d72c0453614942ef7a
addagarlasrujana33-crypto <addagarlasrujana33@gmail.com>
2025-11-30 19:17:49 +0530

    Add files via upload

---
+++ even or odd in if else statement.cpp
@@ -0,0 +1,17 @@
+//to check the given number is even or odd 
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the n value");
+	scanf("%d", &n);
+	if(n%2==0)
+	{
+		printf("n is even number");
+	}
+	else
+	{
+		printf("n is odd number");
+	}
+	return 0;
+}
