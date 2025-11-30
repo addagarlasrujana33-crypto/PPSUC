ad25afafd3b1fa878868e8ae616274ee3994b158
addagarlasrujana33-crypto <addagarlasrujana33@gmail.com>
2025-11-30 20:13:15 +0530

    Add files via upload

---
+++ i value in if else statement.cpp
@@ -0,0 +1,16 @@
+//to perform the i value in if else statement
+#include<stdio.h>
+int main()
+{
+	int i=100;
+	if(i==100)
+	{
+		printf("the i value is %d",100);
+	}
+	else
+	{
+		printf("the i value is not 100");
+	}
+	return 0;
+}
+
